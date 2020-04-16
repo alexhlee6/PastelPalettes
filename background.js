c34f8a3ea1ed2fff5ccd9b8b5ae29dd199d69b8b
Alex Lee <alex629lee@yahoo.com>
2020-04-15 17:29:52 -0700

    Reload page when selection is changed

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
-// 
-
+const setThemeColor = (tabId) => {
+  chrome.storage.sync.get({
+    themeColor: 'pink',
+  }, (items) => {
+    chrome.tabs.insertCSS(tabId, {
+      file: `css/${items.themeColor}.css`
+    });
+  })
+}
 
 var browserListener = function (tabId, changeInfo, tab) {
   var regexPage = new RegExp(/https:\/\/twitter.com\//);
   var match = regexPage.exec(tab.url);
   if (match && tab.status === 'complete') {
-    chrome.tabs.insertCSS(tab.id, {
-      file: "css/pink.css"
-    });
+    setThemeColor(tab.id);
   }
 }
 chrome.tabs.onUpdated.addListener(browserListener);
--- popup/popup.js
+++ popup/popup.js
@@ -13,6 +13,15 @@ function save_options() {
       saveButton.disabled = false;
     }, 750);
   });
+  chrome.tabs.query({ status: 'complete' }, (tabs) => {
+    tabs.forEach((tab) => {
+      let regexPage = new RegExp(/https:\/\/twitter.com\//);
+      let match = regexPage.exec(tab.url);
+      if (match) {
+        chrome.tabs.update(tab.id, { url: tab.url });
+      }
+    });
+  });
 }
 
 // Restores select box and checkbox state using the preferences
