e644ab0d5d2fd598e8b473d95046f0a628932a56
Alex Lee <53023081+alex629lee@users.noreply.github.com>
2024-02-17 16:34:59 -0800

    Fix post button color

--- background.js
+++ background.js
@@ -14,7 +14,7 @@ const setThemeColorInitial = async () => {
   const items = await chrome.storage.sync.get({
     themeColor: 'pink',
     useOriginalFont: false
-  });
+  }).catch(e => console.log(e));
 
   if (items.themeColor === "none") {
     await chrome.scripting.unregisterContentScripts();
@@ -23,26 +23,26 @@ const setThemeColorInitial = async () => {
     await chrome.scripting.registerContentScripts([
       { id: "main", matches: ['https://twitter.com/*'], css: ['css/main.css'], runAt: "document_start" },
       { id: "theme-color", matches: ['https://twitter.com/*'], css: [`css/colors/${items.themeColor}.css`], runAt: "document_start" }
-    ]);
+    ]).catch(e => console.log(e));
   }
 }
 
 
 const doSetup = async () => {
-  const twtTabs = await chrome.tabs.query({ status: 'complete', discarded: false, url: "https://twitter.com/*" });
+  const twtTabs = await chrome.tabs.query({ status: 'complete', discarded: false, url: "https://twitter.com/*" }).catch(e => console.log(e));
 
   for (let i = 0; i < twtTabs.length; i++) {
     const tab = twtTabs[i];
     await chrome.scripting.removeCSS({
       files: OLD_FILES_TO_REMOVE,
       target: { tabId: tab.id }
-    });
+    }).catch(e => console.log(e));
   }
 
   await setThemeColorInitial();
   twtTabs.forEach(tab => {
     if (tab.active) {
-      chrome.tabs.update(tab.id, { url: tab.url });
+      chrome.tabs.update(tab.id, { url: tab.url }).catch(e => console.log(e));
     } else {
       chrome.tabs.discard(tab.id).catch(e => console.log(e));
     }
